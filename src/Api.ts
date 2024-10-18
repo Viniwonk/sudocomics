b11aeeb4445e60f0266efc3be7ff7fce426d0eca
Vinicius <vicimon15@gmail.com>
2024-10-18 13:35:53 -0300

    Requisição Get do sistema de pesquisa e sugestões

--- src/Api.ts
+++ src/Api.ts
@@ -1,153 +1,162 @@
-export const moduleApi ={
-    /* Pesquisa ID */
+export const moduleApi = {
+  /* Pesquisa ID */
 
-    pesquisaID: async() =>{
-        let response = await fetch('');
-        let json = await response.json();
-        return json
-    },
-    
- 
-    /* Quadrinho DM */
-    adicionarQuadrinho: async(edicao:string,colecao:string,lancamento:string,imagemCapa:string, editora:string, uploadedBy:string) =>{
-        let response = await fetch ('',{
-        method:'POST',
-        body:JSON.stringify({
-            edicao,
-            colecao,
-            lancamento,
-            imagemCapa,
-            editora,
-            uploadedBy,
-        }),
-        headers:{'Content-Type':'application/json'}
-    })
+  pesquisaID: async () => {
+    let response = await fetch("");
+    let json = await response.json();
+    return json;
+  },
+  /* Quadrinho DM */
+  adicionarQuadrinho: async (
+    edicao: string,
+    colecao: string,
+    lancamento: string,
+    imagemCapa: string,
+    editora: string,
+    uploadedBy: string
+  ) => {
+    let response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({
+        edicao,
+        colecao,
+        lancamento,
+        imagemCapa,
+        editora,
+        uploadedBy,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
     let json = await response.json();
     console.log(json);
     return json;
-},
-    alteraQuadrinho: async(edicao:string,colecao:string,lancamento:string,imagemCapa:string, editora:string, uploadedBy:string) =>{
-        let response = await fetch ('',{
-        method:'POST',
-        body:JSON.stringify({
-            edicao,
-            colecao,
-            lancamento,
-            imagemCapa,
-            editora,
-            uploadedBy,
-        }),
-        headers:{'Content-Type':'application/json'}
-    })
+  },
+  alteraQuadrinho: async (
+    edicao: string,
+    colecao: string,
+    lancamento: string,
+    imagemCapa: string,
+    editora: string,
+    uploadedBy: string
+  ) => {
+    let response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({
+        edicao,
+        colecao,
+        lancamento,
+        imagemCapa,
+        editora,
+        uploadedBy,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
     let json = await response.json();
     console.log(json);
     return json;
-},
-    removeQuadrinho: async() =>{
-        let response = await fetch('');
-        let json = await response.json();
-        return json
-    },
+  },
+  removeQuadrinho: async () => {
+    let response = await fetch("");
+    let json = await response.json();
+    return json;
+  },
 
-
-
-    /*Editora DM*/
-    adcionarEditora:async(nome:string,logo:string,quadrinho:string) =>{
-        let response = await fetch ('',{
-        method:'POST',
-        body:JSON.stringify({
-            nome,
-            logo,
-            quadrinho,
-        }),
-        headers:{'Content-Type':'application/json'}
-    })
+  /*Editora DM*/
+  adcionarEditora: async (nome: string, logo: string, quadrinho: string) => {
+    let response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({
+        nome,
+        logo,
+        quadrinho,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
     let json = await response.json();
     console.log(json);
     return json;
-},
-    alteraEditora:async(nome:string,logo:string,quadrinho:string) =>{
-        let response = await fetch ('',{
-        method:'POST',
-        body:JSON.stringify({
-            nome,
-            logo,
-            quadrinho,
-        }),
-        headers:{'Content-Type':'application/json'}
-    })
+  },
+  alteraEditora: async (nome: string, logo: string, quadrinho: string) => {
+    let response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({
+        nome,
+        logo,
+        quadrinho,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
     let json = await response.json();
     console.log(json);
     return json;
-},
-    removeEditora: async() =>{
-        let response = await fetch('');
-        let json = await response.json();
-        return json
-    },
+  },
+  removeEditora: async () => {
+    let response = await fetch("");
+    let json = await response.json();
+    return json;
+  },
 
-    /*Autor DM*/
+  /*Autor DM*/
+  adcionarAutor: async (nome: string, foto: string, quadrinho: string) => {
+    let response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({
+        nome,
+        foto,
+        quadrinho,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    let json = await response.json();
+    console.log(json);
+    return json;
+  },
+  alteraAutor: async (nome: string, foto: string, quadrinho: string) => {
+    let response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({
+        nome,
+        foto,
+        quadrinho,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    let json = await response.json();
+    console.log(json);
+    return json;
+  },
+  removeAutor: async () => {
+    let response = await fetch("");
+    let json = await response.json();
+    return json;
+  },
 
-    adcionarAutor:async(nome:string,foto:string,quadrinho:string) =>{
-        let response = await fetch ('',{
-        method:'POST',
-        body:JSON.stringify({
-            nome,
-            foto,
-            quadrinho,
-        }),
-        headers:{'Content-Type':'application/json'}
-    })
+  /*Admin DM*/
+  adcionarAdmin: async (nome: string, email: string, senha: string) => {
+    let response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({
+        nome,
+        email,
+        senha,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
     let json = await response.json();
     console.log(json);
     return json;
-},
-    alteraAutor:async(nome:string,foto:string,quadrinho:string) =>{
-        let response = await fetch ('',{
-        method:'POST',
-        body:JSON.stringify({
-            nome,
-            foto,
-            quadrinho,
-        }),
-        headers:{'Content-Type':'application/json'}
-    })
+  },
+  loginAdmin: async (email: string, senha: string) => {
+    let response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        senha,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
     let json = await response.json();
     console.log(json);
     return json;
-},
-    removeAutor: async() =>{
-        let response = await fetch('');
-        let json = await response.json();
-        return json
-    },
-
-    /*Admin DM*/
-    adcionarAdmin:async(nome:string,email:string,senha:string) =>{
-        let response = await fetch ('',{
-        method:'POST',
-        body:JSON.stringify({
-            nome,
-            email,
-            senha,
-        }),
-        headers:{'Content-Type':'application/json'}
-    })
-    let json = await response.json();
-    console.log(json);
-    return json;
-},
-    loginAdmin:async(email:string,senha:string) =>{
-        let response = await fetch ('',{
-            method:'POST',
-            body:JSON.stringify({
-                email,
-                senha,
-                }),
-                headers:{'Content-Type':'application/json'}
-        })
-        let json = await response.json();
-    console.log(json);
-    return json;
-    }
-}
+  },
+};
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,10 @@
-// Importações Bibliotecas
 import FeatherIcon from "feather-icons-react";
 import { Link, useNavigate } from "react-router-dom";
-// Importações CSS
 import "../../style/App.css";
-// Importações Componentes
 import Tape from "../tape/tape";
-// Importações Imagens
 import logo from "../../images/logo.webp";
 import { useEffect, useState } from "react";
-import { link } from "fs";
+import { moduleApi } from "../../Api";
 
 export default function Header() {
   interface Item {
@@ -22,113 +18,80 @@ export default function Header() {
   const [sugestoes, setSugestoes] = useState<Item[]>([]);
   const navigate = useNavigate();
 
-  const ListaQuad: Item[] = [
-    {
-      id: "dc1",
-      name: "Batman Vol. 2: The Bat-Man of Gotham TP",
-      url: "/quadrinhos/Batman.jpg",
-    },
-    {
-      id: "dc2",
-      name: "Batman / Superman: Worlds Finest #30",
-      url: "/quadrinhos/bbb.jpg",
-    },
-    { id: "mvl1", name: "Phoenix #2", url: "/quadrinhos/Fenix.jpg" },
-    {
-      id: "mvl2",
-      name: "Miles Morales: Spider-Man Annual #1",
-      url: "/quadrinhos/Miles Morales.jpg",
-    },
-    {
-      id: "mvl3",
-      name: "The Incredible Hulk Epic Collection: Ground Zero TP",
-      url: "/quadrinhos/Hulk.jpg",
-    },
-  ];
+  const handlePesquisa = async () => {
+    const foundItems = await moduleApi.pesquisaID();
 
-  const handlePesquisa = () => {
-    //necessario mudar para buscar no banco de dados
-    const foundItems = ListaQuad.filter((item) =>
-      item.name.toLowerCase().includes(pesquisa.toLowerCase())
-    );
-    setResult(foundItems);
-    navigate("/search-results", { state: { results: foundItems } });
-  };
+    if (foundItems.length) {
+      setResult(foundItems);
+      navigate("/search-results", { state: { results: foundItems } });
+    } else {
+      alert("Nenhum item encontrado");
+    }
 
-  useEffect(() => {
-    const fetchSuggestions = () => {
-      if (pesquisa) {
-        const matchedSuggestions = ListaQuad.filter((item) =>
-          item.name.toLowerCase().includes(pesquisa.toLowerCase())
-        );
-        setSugestoes(matchedSuggestions);
-      } else {
-        setSugestoes([]);
-      }
-    };
+    useEffect(() => {
+      const fetchSuggestions = async () => {
+        if (pesquisa) {
+          const suggestions = await moduleApi.pesquisaID();
+          const matchedSuggestions = suggestions.filter(
+            (item: { name: string }) =>
+              item.name.toLowerCase().includes(pesquisa.toLowerCase())
+          );
+          setSugestoes(matchedSuggestions);
+        } else {
+          setSugestoes([]);
+        }
+      };
 
-    fetchSuggestions();
-  }, [pesquisa]);
+      fetchSuggestions();
+    }, [pesquisa]);
 
-  //usar quando tiver banco de dados
-  /*useEffect(() => {
-    const fetchSugestions = async () => {
-      if (pesquisa) {
-        const response = await fetch("");
-        const data = await response.json();
-        setSugestoes(data);
-      } else {
-        setSugestoes([]);
-      }
-    };
-    fetchSugestions();
-  }, [pesquisa]);*/
-  return (
-    <div className="head_wrapper">
-      <div className="head">
-        <div>
-          <Link to="/">
-            <img id="head_logo" src={logo} alt="" />
-          </Link>
+    return (
+      <div className="head_wrapper">
+        <div className="head">
+          <div>
+            <Link to="/">
+              <img id="head_logo" src={logo} alt="" />
+            </Link>
+          </div>
+          <div>
+            <input
+              className="search-bar"
+              type="text"
+              value={pesquisa}
+              onChange={(e) => setPesquisa(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handlePesquisa();
+                }
+              }}
+              placeholder="Pesquisar"
+            />
+            <button className="search-button" onClick={handlePesquisa}></button>
+            {sugestoes.length > 0 && (
+              <ul className="suggestions-list">
+                {sugestoes.map((suggestion) => (
+                  <Link to={`/catalogue/${suggestion.id}`}>
+                    <li
+                      key={suggestion.id}
+                      onClick={() => setPesquisa(suggestion.name)}
+                    >
+                      {suggestion.name}
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            )}
+          </div>
+          <div className="head_botoes">
+            <Link to={"/admlogin"}>
+              <div className="head_icon">
+                <FeatherIcon icon="user" />
+              </div>
+            </Link>
+          </div>
         </div>
-        <div>
-          <input
-            className="search-bar"
-            type="text"
-            value={pesquisa}
-            onChange={(e) => setPesquisa(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                handlePesquisa();
-              }
-            }}
-            placeholder="Pesquisar"
-          />
-          <button className="search-button" onClick={handlePesquisa}></button>
-          {sugestoes.length > 0 && (
-            <ul className="suggestions-list">
-              {sugestoes.map((suggestion) => (
-                <Link to={`/catalogue/${suggestion.id}`}>
-                  <li
-                    key={suggestion.id}
-                    onClick={() => setPesquisa(suggestion.name)}
-                  >
-                    {suggestion.name}
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div className="head_botoes">
-          <Link to={"/admlogin"}>
-            <div className="head_icon">
-              <FeatherIcon icon="user" />
-            </div>
-          </Link>
-        </div>
+        <Tape></Tape>
       </div>
-      <Tape></Tape>
-    </div>
-  );
+    );
+  };
 }
